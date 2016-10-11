9d54c875ae36f08e37f1bda4c594766579290606
RoxyYao <1175500739@qq.com>
2016-10-11 16:45:15 +0800

    test1

--- src/main/java/com/example/repository/CisCustomerinfoRepository.java
+++ src/main/java/com/example/repository/CisCustomerinfoRepository.java
@@ -1,10 +1,10 @@
 package com.example.repository;
 
-import com.kift.demo.bean.cis.CisCustomerinfo;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
+import com.example.bean.cis.CisCustomerinfo;
 import java.util.List;
 
 
